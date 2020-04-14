244918b8f80f7c1005b38381f8634f618cf63614
MuhammadAnggaSaputra <Manggas703@gmail.com>
2020-04-14 21:44:03 +0700

    delate pertemuan4

--- kuliah/pertemuan4/Tugas4_1.java
+++
@@ -1,9 +0,0 @@
-public class Tugas4_1
-{
-	public void run()
-	{
-		System.out.printf("%9s | %-15s | %-6.1f \n", "193040001", "Asep Saripudin", 3.5);
-		System.out.printf("%9s | %-15s | %-6.1f \n", "193040002", "Jajang Nurjaman", 3.3);
-		System.out.printf("%9s | %-15s | %-6.1f \n", "193040003", "Budiman", 2.7);
-	}
-}	
