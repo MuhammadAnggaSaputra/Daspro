a143dd4ce0b0a0fe88f1849c051572db1ed7e648
MuhammadAnggaSaputra <Manggas703@gmail.com>
2020-04-14 21:45:46 +0700

    menambahakan file 4

---
+++ kuliah/pertemuan4/Utama4_1.java
@@ -0,0 +1,8 @@
+public class Utama4_1
+{
+	public static void main(String[] args)
+	{
+		Tugas4_1 program = new Tugas4_1();
+		program.run();
+	}
+}	
---
+++ kuliah/pertemuan4/Utama4_2.java
@@ -0,0 +1,8 @@
+public class Utama4_2
+{
+	public static void main(String[] args)
+	{
+		Tugas4_2 program = new Tugas4_2();
+		program.run();
+	}
+}	
